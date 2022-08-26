8ea9bfa42cafa7e42b4091f0e2a980dec8cc9c44
endokaho <endokaho0208@gmail.com>
2022-08-26 10:51:22 +0900

    [update] imgから2点画像を削除しました。　imgにきのかホームのロゴを追加しました。　header2.phpの画像を入れ替えました。　dummy.png→kinokahome_logo.png

--- header2.php
+++ header2.php
@@ -8,7 +8,7 @@
 
                     <div class="desk_header_left">
                         <a href="https://kinoka-home.com/"><img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/png/dummy.png" class="logo"></a>
+                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/png/kinokahome_logo.png" class="logo"></a>
                         <p class="header_text_kinoka">四街道市で注文住宅,平屋,新築を建てる<br class="br_sp">工務店ならきのかホーム</p>
 
                         <div class="header_contact">
